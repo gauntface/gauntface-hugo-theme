a58c26bda6a33cc1bf3c1b3c2346baaebdfb198a
gauntface <gauntface@users.noreply.github.com>
2020-04-15 03:49:10 +0000

    deploy: 2bed86ed2dccc9173d7208e6d1f0db2383606af4

---
+++ js/n-hopin-u-async-css-async.8a7dc7d.js
@@ -0,0 +1,2 @@
+!function(){"use strict";window.addEventListener("load",()=>{if(!haCSS)return;const e=document.querySelector("head");for(const t of haCSS){const n=document.createElement("link");n.href=t,n.rel="stylesheet",e.appendChild(n)}})}();
+//# sourceMappingURL=n-hopin-u-async-css-async.js.map
---
+++ js/n-hopin-u-async-css-async.d.ts
@@ -0,0 +1,1 @@
+declare const haCSS: Array<string>;
