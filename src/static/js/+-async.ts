4b792292486a898e553f90e03e6d76c2ba1eb205
Matt Gaunt <mattgaunt@google.com>
2020-04-19 08:05:42 -0700

    Small cleanup of logic and updates for html asset injector

--- src/static/js/+-async.ts
+++ src/static/js/+-async.ts
@@ -1,4 +1,6 @@
-window.addEventListener('load', () => {
+declare const haCSS: Array<string>;
+
+function analytics() {
   (<any>window).dataLayer = [];
 
   function gtag(...any: (string | Date)[]) {
@@ -11,4 +13,31 @@ window.addEventListener('load', () => {
   s.src = "https://www.googletagmanager.com/gtag/js?id=UA-161686512-1";
   s.async = true;
   s.defer = true;
+}
+
+function asyncCSS() {
+  if (!('haCSS' in window)) {
+    return
+  }
+
+  const head = document.querySelector('head');
+  for (const c of haCSS) {
+    const l = document.createElement('link');
+    l.href = c;
+    l.rel = "stylesheet";
+    head.appendChild(l);
+  }
+}
+
+function asyncDataSrc() {
+  const elements = document.querySelectorAll<HTMLIFrameElement>("[data-src]");
+  for (const e of elements) {
+    e.src = e.dataset.src;
+  }
+}
+
+window.addEventListener('load', () => {
+  analytics();
+  asyncCSS();
+  asyncDataSrc();
 })
